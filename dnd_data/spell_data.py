9cd67860ced899c6e3643ae395c80d41dba97cae
Scott Turnbull <streamweaver@gmail.com>
2021-04-10 14:24:29 -0400

    Added by class aggregation for spells too.

--- dnd_data/spell_data.py
+++ dnd_data/spell_data.py
@@ -6,6 +6,12 @@ class SpellList:
 
     def __init__(self):
         self.spells = []
+        self.level_fieldnames = ['type', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "total"]
+        class_list = sorted(['Bard', 'Druid', 'Ranger', 'Sorcerer', 'Warlock', 'Wizard',
+                                 'Cleric', 'Paladin', 'Artificer', 'Monk'])
+        class_list.insert(0, 'type')
+        class_list.append('total')
+        self.class_fieldnames = class_list
 
     def _add_spells(self, data):
         """
@@ -36,12 +42,12 @@ class SpellList:
     def calculate_condition_types(self):
         return self.calculate_inflict('conditionInflict')
 
-    def write_inflict_type_csv(self, filepath, data):
-        fieldnames = ['type', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "total"]
+    def write_inflict_type_csv(self, filepath, data, fieldnames):
         with open(filepath, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames)
             writer.writeheader()
-            for k, row in data.items():
+            for k, raw_row in data.items():
+                row = {key: value for key, value in raw_row.items() if key in fieldnames}
                 row['type'] = k.title()
                 writer.writerow(row)
 
@@ -51,9 +57,15 @@ class SpellList:
             for inflict in spell.get(inflict_type, []):
                 if inflict not in inflict_totals:
                     inflict_totals[inflict] = dict()
-                inflict_totals[inflict][spell["level"]] = \
-                    inflict_totals[inflict].get(spell["level"], 0) + 1
+                self._increment_level(inflict_totals[inflict], spell["level"])
+                self._increment_class(inflict_totals[inflict], spell["classes"])
                 inflict_totals[inflict]["total"] = \
                     inflict_totals[inflict].get("total", 0) + 1
         return inflict_totals
 
+    def _increment_level(self, d, lvl):
+        d[lvl] = d.get(lvl, 0) + 1
+
+    def _increment_class(selfd, d, classes):
+        for character_class in classes.get('fromClassList', []):
+            d[character_class["name"]] = d.get(character_class["name"], 0) + 1
